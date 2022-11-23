2c44adedaac8f7eb3ad7dbc9d3d788a46b152855
Orest Kashchiy <kashchiy@initservice.com.ua>
2022-11-23 12:30:21 +0200

    test

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-//export const PROD_DATABASE = process.env.PROD_DATABASE
 
 mongoose.connect('mongodb+srv://admin:admintest@demo-cluster.kobxizl.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
